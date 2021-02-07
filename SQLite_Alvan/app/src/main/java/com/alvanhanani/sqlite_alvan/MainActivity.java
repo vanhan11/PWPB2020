160cfce2dbd44da7d1555a91f66a1b42a3280af1
vanhan200902 <vanhan200902@gmail.com>
2021-02-07 14:05:07 +0700

    SQL Lite

--- SQLite_Alvan/app/src/main/java/com/alvanhanani/sqlite_alvan/MainActivity.java
+++ SQLite_Alvan/app/src/main/java/com/alvanhanani/sqlite_alvan/MainActivity.java
@@ -3,6 +3,10 @@ package com.alvanhanani.sqlite_alvan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment fragment=new MainFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.framecontainer,fragment);
+        fragmentTransaction.commit();
     }
 }
